3dd3b37c969b84a9f6b320b0a9711d7cc2c50a11
Wale Ayandiran (walecloud) <walecloud@gmail.com>
2022-06-07 15:08:49 +0100

    streamlit

---
+++ app.py
@@ -0,0 +1,123 @@
+import numpy as np
+import streamlit as st
+import cv2 as cv
+from PIL import Image
+from keras.models import load_model
+from tensorflow.keras import preprocessing
+
+sign_labels = {
+  0: 'Speed limit (20km/h)',
+  1: 'Speed limit (30km/h)',
+  2: 'Speed limit (50km/h)',
+  3: 'Speed limit (60km/h)',
+  4: 'Speed limit (70km/h)',
+  5: 'Speed limit (80km/h)',
+  6: 'End of speed limit (80km/h)',
+  7: 'Speed limit (100km/h)',
+  8: 'Speed limit (120km/h)',
+  9: 'No passing',
+  10: 'No passing vehicle over 3.5 tons',
+  11: 'Right-of-way at intersection',
+  12: 'Priority road',
+  13: 'Yield',
+  14: 'Stop',
+  15: 'No vehicles',
+  16: 'Vehicle > 3.5 tons prohibited',
+  17: 'No entry',
+  18: 'General caution',
+  19: 'Dangerous curve left',
+  20: 'Dangerous curve right',
+  21: 'Double curve',
+  22: 'Bumpy road',
+  23: 'Slippery road',
+  24: 'Road narrows on the right',
+  25: 'Road work',
+  26: 'Traffic signals',
+  27: 'Pedestrians',
+  28: 'Children crossing',
+  29: 'Bicycles crossing',
+  30: 'Beware of ice/snow',
+  31: 'animals crossing',
+  32: 'End speed + passing limits',
+  33: 'Turn right ahead',
+  34: 'Turn left ahead',
+  35: 'Ahead only',
+  36: 'Go straight or right',
+  37: 'Go straight or left',
+  38: 'Keep right',
+  39: 'Keep left',
+  40: 'Roundabout mandatory',
+  41: 'End of no passing',
+  42: 'End no passing vehicle > 3.5 tons'
+}
+
+from keras.models import load_model
+from keras.preprocessing import image
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+
+def load_image(img, show=False):
+
+  img_tensor = image.img_to_array(img)                    # (height, width, channels)
+  img_tensor = np.expand_dims(img_tensor, axis= 0)         # (1, height, width, channels), add a dimension because the model expects this shape: (batch_size, height, width, channels)
+  img_tensor /= 255.                                      # imshow expects values in the range [0, 1]
+
+  return img_tensor
+
+@st.cache
+def sign_predict(image):
+  # model = load_model('./cnn_model/')
+  model = load_model("./cnn_model.h5")
+
+  prediction = model.predict_classes(image)
+  
+  return prediction
+
+
+def main():
+  st.title('Traffic Signs Classifier')
+  st.markdown("""
+      This application classifies traffic signs. Upload any photo of a traffic sign 
+      and receive its name out of 43 present classes. For getting the correct prediction, 
+      try to upload a square picture containing only the sign.
+      """)
+  with st.expander("See list of classes"):
+    st.write(list(sign_labels.values()))
+  
+  uploaded_image = st.file_uploader('Upload a photo of traffic sign here', type=['jpg', 'jpeg', 'png'])
+
+  if uploaded_image is not None:
+    col1, col2 = st.columns(2)
+    col1.markdown('#### Your picture ####')
+    # image = Image.open(uploaded_image)
+    with col1:
+      # Convert the file to an opencv image.
+      file_bytes = np.asarray(bytearray(uploaded_image.read()), dtype=np.uint8)
+      opencv_image = cv.imdecode(file_bytes, 1)
+      resized_image = cv.resize(opencv_image, (50, 50))
+      print('Resized Dimensions : ',resized_image.shape)
+      # Now do something with the image! For example, let's display it:
+      st.image(resized_image, use_column_width=True, channels="BGR")
+      # st.image(image, use_column_width=True)
+    # print(image)
+    procesed_image = load_image(resized_image)
+
+    # Make prediction
+    prediction_label= sign_predict(procesed_image)
+    print('prediction_label----', prediction_label[0])
+    st.write('### Prediction:')
+    st.write(sign_labels[prediction_label[0]])
+    # st.write('##### Confidence:', str(confidence))
+    st.markdown('***')
+
+    # Markdowns
+    # st.subheader('About')
+    # st.markdown("""
+    # The app uses an implementation of AlexNet Convolutional Neural Network. 
+    # """)
+
+
+if __name__ == '__main__':
+  main()
